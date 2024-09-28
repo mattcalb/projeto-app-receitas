125171a4d5e14d8684752f1f1d6b00fda9ccc99d
Matthäus Campanher Albrecht <mattcalb1@gmail.com>
2024-09-28 16:41:38 -0300

    inicio tela home

--- app/src/main/java/com/example/projetoappreceitas/MainActivity.kt
+++ app/src/main/java/com/example/projetoappreceitas/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.projetoappreceitas
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val loginButton = findViewById<Button>(R.id.login_button)
+        val emailInput = findViewById<EditText>(R.id.email)
+        val passwordInput = findViewById<EditText>(R.id.password)
+        loginButton.setOnClickListener {
+            if (emailInput.text.toString().equals("user@gmail.com") &&
+                passwordInput.text.toString().equals("1234")) {
+                Toast.makeText(this, "Logado com sucesso!", Toast.LENGTH_LONG).show()
+                val intent = Intent(applicationContext, Home::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
